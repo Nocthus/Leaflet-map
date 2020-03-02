5dc927d9c97ffc737f2817cacc2c10f87e138c09
Nocthus <58665599+Nocthus@users.noreply.github.com>
2020-03-02 12:39:54 -0300

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,13 +29,25 @@ export class HomePage {
       maxZoom: 20
     }).on('locationfound', (e) => {
       let markerGroup = leaflet.featureGroup();
-      let marker: any = leaflet.marker([-3.76882255, -38.47772811]).on('click', () => {
-        alert('Marker clicked');   
-      })
-      let marker2: any = leaflet.marker([-3.76888876, -38.47862452]);
+      let marker: any = leaflet.marker([-3.76882255, -38.47772811]).bindPopup('DCE').openPopup();   
+      let marker2: any = leaflet.marker([-3.76888876, -38.47862452]).bindPopup('Bloco M').openPopup();
+      let marker3: any = leaflet.marker([-3.77025900, -38.48157500]).bindPopup('Bloco A').openPopup();
+      let marker4: any = leaflet.marker([-3.770709, -38.481398]).bindPopup('Bloco B').openPopup();
+      let marker5: any = leaflet.marker([-3.770527, -38.480448]).bindPopup('Bloco B').openPopup();
+      let marker6: any = leaflet.marker([-3.767947, -38.480540]).bindPopup('Bloco B').openPopup();
+      let marker7: any = leaflet.marker([-3.766780, -38.479730]).bindPopup('Bloco B').openPopup();
+      let marker8: any = leaflet.marker([-3.769526, -38.478866]).bindPopup('Bloco B').openPopup();
+      let marker9: any = leaflet.marker([-3.767465, -38.479424]).bindPopup('Bloco B').openPopup();
       let polyline: any = leaflet.polyline([[-3.76888876, -38.47862452],[-3.76882255, -38.47772811]]);
       markerGroup.addLayer(marker);
       markerGroup.addLayer(marker2);
+      markerGroup.addLayer(marker3);
+      markerGroup.addLayer(marker4);
+      markerGroup.addLayer(marker5);
+      markerGroup.addLayer(marker6);
+      markerGroup.addLayer(marker7);
+      markerGroup.addLayer(marker8);
+      markerGroup.addLayer(marker9);
       markerGroup.addLayer(polyline);
       this.map.addLayer(markerGroup);
       }).on('locationerror', (err) => {
